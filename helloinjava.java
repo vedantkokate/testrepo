7829fb4770da14064573e541b0e1600bb8db6dd8
prasuk1 <43782381+prasuk1@users.noreply.github.com>
2018-10-30 10:10:30 +0530

    Update helloinjava.java

--- helloinjava.java
+++ helloinjava.java
@@ -3,6 +3,12 @@ public class trying
 public static void main()
 {
 System.out.println("Helo World");
+  for(int =0;i<10;i++)
+   System.out.println(i); 
+      
+      
+      
+  
 }
 }
 //i don't get this
